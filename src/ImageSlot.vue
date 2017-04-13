b06c17f432b288849a9d4675c7110a3e05e602c9
kikyous <kikyous@163.com>
2017-04-13 12:03:05 +0800

    image upload

--- src/ImageSlot.vue
+++ src/ImageSlot.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class='solt' :style="styleObject">
-    {{layout.id}}
+  <div class='solt' :style="styleObject" @click='choosefile'>
+    <span class='placeholder' v-if='!src'>+</span>
+    <vue-core-image-upload
+      v-bind:class="['hide']" 
+      v-bind:crop="false" url="https://sm.ms/api/upload"
+                          extensions="png,gif,jpeg,jpg"
+                          inputOfFile='smfile'
+                          text=''
+                          v-on:imageuploaded="imageuploaded">
+    </vue-core-image-upload>
+    <img :src='src' :style="imgStyle">
   </div>
 </template>
 
 <script>
+import VueCoreImageUpload  from 'vue-core-image-upload';
 export default {
   props: ['layout', 'scale'],
   name: 'image-solt',
   data () {
     return {
+      src: null,
+      imgStyle: {}
+    }
+  },
+  components: {
+    VueCoreImageUpload
+  },
+  methods: {
+    imageuploaded(res) {
+      if (res.code == 'success') {
+        if ((res.data.width / res.data.height) > (this.$el.offsetWidth / this.$el.offsetHeight)){
+          this.imgStyle['height'] = '100%';
+        } else {
+          this.imgStyle['width'] = '100%';
+        }
+        this.src = res.data.url;
+      }
+    },
+    choosefile(){
+      this.$el.querySelector('input').click();
     }
   },
   computed: {
@@ -31,5 +61,9 @@ export default {
 .solt{
   position: absolute;
   border:1px dashed #000;
+  overflow: hidden;
+}
+.placeholder{
+  font-size: 50px;
 }
 </style>
